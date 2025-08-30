a333fdca081fac4b4a86212fb92b65190b6c62fe
Sebastian Galindo Zaragoza <133563476+Sebastian-Zaragoza@users.noreply.github.com>
2025-08-30 13:54:43 -0600

    feat: Changes applied

--- src/views/PortfolioView.tsx
+++ src/views/PortfolioView.tsx
@@ -1,7 +1,7 @@
 export default function PortfolioView() {
     return (
             <div className="flex h-screen items-center justify-center">
-                <label className="font-semibold text-2xl font-sans text-center"> Hola Zaragoza Sebastian</label>
+                <label className="font-semibold text-2xl font-sans text-center"> Hi Zaragoza Sebastian</label>
             </div>
     );
 }
