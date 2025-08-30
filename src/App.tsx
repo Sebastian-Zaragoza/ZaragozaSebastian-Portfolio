ad6599ade412d9be4a7c2f198e7d3a98656c2379
Sebastian Galindo Zaragoza <133563476+Sebastian-Zaragoza@users.noreply.github.com>
2025-08-30 13:16:13 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <label className="font-semibold text-2xl font-sans text-center"> Hola Zaragoza Sebastian</label>
+        </div>
+    );
+}
+
